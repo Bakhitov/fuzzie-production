13079043e78c31d1d89f45d4bafee32bcee10c1a
Bakhitov akhan <bakhitov.akhan@gmail.com>
2024-07-14 20:43:23 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,13 +36,7 @@ export default function Home() {
           />
         </div>
       </section>
-      <InfiniteMovingCards
-        className="md:mt-[18rem] mt-[-100px]"
-        items={clients}
-        direction="right"
-        speed="slow"
-      />
-      <section className="mt-[-1500px]">
+      <section className="mt-[0px]">
         <LampComponent />
         <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
           <CardContainer className="inter-var ">
